b7c0e29da214c26b81a9e38fb9286fef456d57f4
pishpishi <pishro@duck.com>
2025-02-17 06:36:33 +0330

    Last

--- src/hooks/usePB.ts
+++ src/hooks/usePB.ts
@@ -4,7 +4,7 @@ let pb: PocketBase | undefined = undefined
 
 export default function usePB(): PocketBase {
     if (!pb) {
-        pb = new PocketBase('http://127.0.0.1:8090');
+        pb = new PocketBase('https://pocket.bimarznet.com');
     }
     return pb;
 }
